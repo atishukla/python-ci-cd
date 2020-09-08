604a0914cecfdc0765dbd72ca5685c8bd61f4f10
atishukla <atishayshukla@gmail.com>
2020-09-08 20:37:27 +0200

    removed input file param from pipeline

--- pipelines/build.groovy
+++ pipelines/build.groovy
@@ -4,9 +4,9 @@ node() {
 	stage ('Checkout') {
 	    // the data for sample and csv was incorrect clear workspace
 	    deleteDir()
-		def inputFile = input message: 'Upload file', parameters: [file(name: 'sample-2.txt')]
-        new hudson.FilePath(new File("$workspace/resources/sample-2.txt")).copyFrom(inputFile)
-        inputFile.delete()
+		// def inputFile = input message: 'Upload file', parameters: [file(name: 'sample-2.txt')]
+        // new hudson.FilePath(new File("$workspace/resources/sample-2.txt")).copyFrom(inputFile)
+        // inputFile.delete()
 	    checkout scm
 	}
 
